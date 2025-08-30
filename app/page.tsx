dbaf2cf542199daeae236dd5f343af951d087083
Pedro Pablo Osorio San Martin <pedro.osorio04@inacapmail.com>
2025-08-30 15:05:24 -0400

    Se configura el sistema, creacion de proyecto, instalacion de paquetes y dependencias, mas ejecucion de primera pagina

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <>
+      <div>
+        <h1 className="text-2xl underline text-white">Home</h1>
+        <Button>Click Me</Button>
+      </div>
+    </>
+  )
+}
