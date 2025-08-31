0df21c23462a1994af407781f88706f98323580a
Pedro Pablo Osorio San Martin <pedro.osorio04@inacapmail.com>
2025-08-30 22:18:16 -0400

    Se trabaja en design de HomePage o Pagina Principal

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,8 @@ import type { Metadata } from "next";
 import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
 
-import {cn} from "@/lib/utils";
+import { cn } from "@/lib/utils";
+import { ThemeProvider } from "next-themes";
 
 const fontSans = Plus_Jakarta_Sans({
   subsets: ["latin"],
@@ -21,9 +22,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={cn("min-h-screen bg-dark-300 font-sans antialiased", fontSans.variable)}>
-        {children}
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={cn(
+          "min-h-screen bg-dark-300 font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
