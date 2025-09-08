df82a7932d3a61ba7a6c23b5bbb6c564591a149c
Pedro Pablo Osorio San Martin <pedro.osorio04@inacapmail.com>
2025-09-08 15:45:07 -0300

    se trabaja en el login y registro de usuario, ahora de momento si funciona

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -9,8 +9,7 @@ import SubmitButton from "../SubmitButton";
 import { useState } from "react"
 import UserFormValidation from "@/lib/validation"
 import { useRouter } from "next/navigation"
-import { createUser } from "@/lib/ actions/patient.actions"
-
+import { createUser } from "@/lib/actions/patient.actions"
 
 export enum FormFieldType {
   INPUT = "input",
@@ -40,13 +39,8 @@ const PatientForm = () => {
     setIsLoading(true);
 
     try {
-      const userData = {
-        name,
-        email,
-        phone,
-      };
+      const userData = { name, email, phone };
       const user = await createUser(userData);
-
       if(user) router.push(`/patients/${user.$id}/register`);
     } catch (error) {
       console.log(error)
