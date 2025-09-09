b935f7ea42d6ce72e9af85f991fd7455721d0274
Pedro Pablo Osorio San Martin <pedro.osorio04@inacapmail.com>
2025-09-09 15:37:22 -0300

    Se avanza en la pagina de registro de usuarios, esta costando si xD

--- app/patients/[userId]/register/page.tsx
+++ app/patients/[userId]/register/page.tsx
@@ -2,38 +2,52 @@ import React from 'react'
 import Image from 'next/image'
 import RegisterForm from '@/components/forms/RegisterForm'
 //import { getUser } from '@/lib/actions/patient.actions'
+import Link from 'next/link'
+import { getUser } from '@/lib/actions/patient.actions'
 
+const Register = async ({ params: { userId } }: SearchParamProps) => {
 
-const Register = async ({params: {userId}}: SearchParamProps) => {
-
-    //const user = await getUser(userId);
+    const user = await getUser(userId);
 
     return (
-        <div className="flex h-screen max-h-screen">
-            <section className="remove-scrollbar container">
-                <div className="sub-container max-w-[860px] flex-1 flex-col py-10">
-                    <Image
-                        src="/assets/icons/logo-full.svg"
-                        height={1000}
-                        width={1000}
-                        alt="patient"
-                        className="mb-12 h-10 w-fit"
-                    />
+        <>
+            <div className="flex h-screen max-h-screen">
 
-                    <RegisterForm  />
+                {/* TODO: OTP Verification | PasskeyModal  */}
 
-                    <p className="copyright py-12">© 2024 CarePluse All rights reserved.</p>
-                </div>
-            </section>
+                <section className="remove-scrollbar container my-auto">
+                    <div className="sub-container max-w-[496px]">
+                        <Image
+                            src="/assets/icons/logo-full.svg"
+                            height={1000}
+                            width={1000}
+                            alt="patient"
+                            className="mb-12 h-10 w-fit"
+                        />
 
-            <Image
-                src="/assets/images/register-img.png"
-                height={1000}
-                width={1000}
-                alt="patient"
-                className="side-img max-w-[390px]"
-            />
-        </div>
+                        <RegisterForm user={user}/>
+
+                        <div className="text-14-regular mt-20 flex justify-between">
+                            <p className="justify-items-end text-dark-600 xl:text-left">
+                                © 2025 CarePlus. All rights reserved.
+                            </p>
+
+                            <Link href="/?admin=true" className="text-green-500">
+                                Admin
+                            </Link>
+                        </div>
+                    </div>
+                </section>
+
+                <Image
+                    src="/assets/images/register-img.png"
+                    height={1000}
+                    width={1000}
+                    alt="patient"
+                    className="side-img max-w-[50%]"
+                />
+            </div>
+        </>
     )
 }
 
